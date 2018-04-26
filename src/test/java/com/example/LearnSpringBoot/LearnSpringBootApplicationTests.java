9304a9812405b1eb5b94e178ee46fe216ef762bb
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-26 13:58:04 +0800

    add helloController home test

--- src/test/java/com/example/LearnSpringBoot/LearnSpringBootApplicationTests.java
+++ src/test/java/com/example/LearnSpringBoot/LearnSpringBootApplicationTests.java
@@ -1,16 +1,32 @@
 package com.example.LearnSpringBoot;
 
+import controller.HelloController;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LearnSpringBootApplicationTests {
+	private MockMvc mMockMvc;
 
+	@Before
+	public void setUp() {
+		mMockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		mMockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("Hello World!")));
 	}
 
 }
