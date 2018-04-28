71158f0fd58239b6bf6f61fce23e62f2b8914911
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-29 01:26:32 +0800

    forget to commit

--- src/main/java/com/example/LearnSpringBoot/LearnSpringBootApplication.java
+++ src/main/java/com/example/LearnSpringBoot/LearnSpringBootApplication.java
@@ -1,5 +1,6 @@
 package com.example.LearnSpringBoot;
 
+import controller.EmployeeController;
 import controller.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloController.class, args);
+		SpringApplication.run(EmployeeController.class, args);
 	}
 
 
--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,4 +1,58 @@
 package controller;
 
+import model.Employee;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping(value = "/employees")
 public class EmployeeController {
+
+    // 创建线程安全的Map
+    static Map<Long, Employee> employeeMap = Collections.synchronizedMap(new HashMap<Long, Employee>());
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public List<Employee> getEmployeeList() {
+        List<Employee> employees = new ArrayList<Employee>(employeeMap.values());
+        return employees;
+    }
+
+    @RequestMapping(value="/add", method=RequestMethod.POST)
+    public String addEmployee(@ModelAttribute Employee employee) {
+        if (employeeMap.containsKey(employee.getId())) {
+            return "add fail, the employee has existed";
+        }
+        employeeMap.put(employee.getId(), employee);
+        return "success";
+    }
+
+    @RequestMapping(value="/get/{id}", method=RequestMethod.GET)
+    public Employee getEmployee(@PathVariable Long id) {
+        return employeeMap.get(id);
+    }
+
+    @RequestMapping(value="/update/{id}", method=RequestMethod.PUT)
+    public String updateEmployee(@PathVariable Long id, @ModelAttribute Employee employee) {
+        Employee employee1 = employeeMap.get(id);
+        if (employee1 != null) {
+            employee1.setName(employee.getName());
+            employee1.setAge(employee.getAge());
+            employee1.setGender(employee.getGender());
+            employeeMap.put(id, employee1);
+            return "success";
+        }
+        return "update fail, the employee does not exist!";
+    }
+
+    @RequestMapping(value="/delete/{id}", method=RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable Long id) {
+        if (employeeMap.containsKey(id)) {
+            employeeMap.remove(id);
+            return "success";
+        }
+        return "delete fail, please verify the id!";
+    }
 }
